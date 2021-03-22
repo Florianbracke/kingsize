504ac42cc09d605760efaeabbeba6bc993dc5c02
Florianbracke <florianbracke@gmail.com>
2021-03-22 16:51:28 +0100

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GeolocationComponent } from './geolocation/geolocation.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import {AboutusComponent} from './aboutus/aboutus.component';
+import {LoginComponent} from './login/login.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: ''                , component: HomepageComponent},
+  { path: 'Geolocation'     , component: GeolocationComponent},
+  { path: 'About-us', component: AboutusComponent},
+  { path: 'Login', component: LoginComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
