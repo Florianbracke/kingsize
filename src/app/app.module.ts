13619862a5324ead8689d3d61c7093ab0597718b
Fran1409 <fran_andries@hotmail.com>
2021-03-22 14:48:48 +0100

    Add homepage component + material modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,38 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GeolocationComponent } from './geolocation/geolocation.component';
 import { HeaderComponent } from './header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RippleComponent } from './ripple/ripple.component';
+import { FormsModule } from '@angular/forms'; 
+import { ReactiveFormsModule } from '@angular/forms';
+
+//Material components
+import { MatRippleModule } from '@angular/material/core';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { HomepageComponent } from './homepage/homepage.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     GeolocationComponent,
     HeaderComponent,
+    RippleComponent,
+    HomepageComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatRippleModule,
+    MatCheckboxModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
