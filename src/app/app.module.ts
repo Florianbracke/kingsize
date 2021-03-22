5dc5dc84db9a80025bb3a1c6e825ad06735bf36f
Florianbracke <florianbracke@gmail.com>
2021-03-22 12:12:25 +0100

    update header to Angular Material icon buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GeolocationComponent } from './geolocation/geolocation.component';
 import { HeaderComponent } from './header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     GeolocationComponent,
     HeaderComponent,
+    
   ],
+ 
   imports: [
     BrowserModule,
     AppRoutingModule,
-    
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
