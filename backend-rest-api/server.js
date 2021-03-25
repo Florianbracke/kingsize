f02eb68842fc13f294dab4c0df895b6037896cc9
Fran1409 <fran_andries@hotmail.com>
2021-03-25 11:36:39 +0100

    Add different functions API

--- backend-rest-api/server.js
+++ backend-rest-api/server.js
@@ -21,3 +21,23 @@ app.route('api/dogs/:name').get((req,res) => {
 
 //sending a new object to an endpoint (to insert a new dog into the database)
 
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+app.route('api/dogs').post((req,res) => {
+    res.send(201, req.body);
+});
+
+//changing an object at an endpoint
+
+app.route('/api/dogs/:name').put((req, res) => {
+    res.send(200, req.body)
+});
+
+//deleting an object at an endpoint
+
+app.route('/api/dogs/:name').delete((req, res) => {
+    res.sendStatus(204)
+});
+
