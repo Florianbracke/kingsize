1f65f82a73a986b89c6f5490d9a669ff8a9b5256
Fran1409 <fran_andries@hotmail.com>
2021-03-29 12:00:56 +0200

    Add basics styling and html for matches

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { GeolocationComponent } from './geolocation/geolocation.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import {AboutusComponent} from './aboutus/aboutus.component';
 import {LoginComponent} from './login/login.component'
+import { MatchesComponent } from './matches/matches.component';
 
 const routes: Routes = [
 
@@ -11,6 +12,7 @@ const routes: Routes = [
   { path: 'Geolocation'     , component: GeolocationComponent},
   { path: 'About-us'        , component: AboutusComponent},
   { path: 'Login'           , component: LoginComponent},
+  { path: 'Matches'         , component: MatchesComponent},
   
   ];
 
