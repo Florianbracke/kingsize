e1604ed0059d9cbcaba23b979296c60640100a70
Fran1409 <fran_andries@hotmail.com>
2021-03-31 13:55:30 +0200

    Add questions for profile page if dog owner or walker

--- frontend/src/app/profile/profile.component.ts
+++ frontend/src/app/profile/profile.component.ts
@@ -12,11 +12,24 @@ export class ProfileComponent implements OnInit {
   profileForm = this.fb.group({
     firstName : ['', Validators.required],
     lastName : ['', Validators.required],
+    dogOwner : [false],
+    dogWalker : [false],
     adress : this.fb.group({
       street : ['', Validators.required],
       number : ['', Validators.required],
       zipcode : ['', Validators.required],
       city : ['', Validators.required],
+    }),
+    dogProfile : this.fb.group({
+      dog : [''],
+      dogName : [''],
+      dogAge : [''],
+      children : [''],
+      leash : [''],
+    }),
+    walkerProfile : this.fb.group({
+      breed : [''],
+      experience : [''],
     }),
   });
 
