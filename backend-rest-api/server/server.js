6f3baf31f790c6d67f63ba3c333dbae408c15317
Florianbracke <florianbracke@gmail.com>
2021-04-02 15:07:19 +0200

    add mysql pool

--- backend-rest-api/server/server.js
+++ backend-rest-api/server/server.js
@@ -1,15 +1,41 @@
 const cors = require('cors')
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+
+const pool= mysql.createPool({
+    connectionLimit : 10,
+    host            : 'localhost',
+    user            :   'root',
+    password        :   'root',
+    database        :   'kingsize'
+})
 
 var corsOptions = {
-  origin: 'http://example.com',
+  origin: true,
+  methods: ["GET","POST"],
+  credentials: true,
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
 }
 
+app.use(express.json());
+
+app.use(express.urlencoded({
+      extended: true
+}));
+  
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:8000");
+    res.setHeader('Acces-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Acces-Contorl-Allow-Methods','Content-Type','Authorization');
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
+
 app.use(cors(corsOptions))
-
-const express = require('express');
-
-const app = express();
 
 app.listen(8000, () => {
     console.log('Server started!')
@@ -25,8 +51,6 @@ app.route('/:name').get((req,res) => {
     const requestedDogName = req.params['name'];
     res.send({ name: requestedDogName});
 });
-
-
 
 //sending a new object to an endpoint (to insert a new dog into the database)
 
