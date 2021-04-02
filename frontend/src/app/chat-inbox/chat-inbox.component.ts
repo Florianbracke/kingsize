4964c723540f0d3068d58e6707ea135c23131291
Fran1409 <fran_andries@hotmail.com>
2021-04-02 15:56:06 +0200

    Add working chatbox

--- frontend/src/app/chat-inbox/chat-inbox.component.ts
+++ frontend/src/app/chat-inbox/chat-inbox.component.ts
@@ -20,10 +20,27 @@ export class ChatInboxComponent implements OnInit {
 
   setupSocketConnection(){
     this.socket = io(SOCKET_ENDPOINT);
+    this.socket.on('message-broadcast', (data: string) => {
+      if (data) {
+       const element = document.createElement('li');
+       element.innerHTML = data;
+       element.style.background = 'white';
+       element.style.padding =  '15px 30px';
+       element.style.margin = '10px';
+       document.getElementById('message-list').appendChild(element);
+       }
+     });
   }
 
   SendMessage() {
     this.socket.emit('message', this.message);
+    const element = document.createElement('li');
+    element.innerHTML = this.message;
+    element.style.background = 'white';
+    element.style.padding =  '15px 30px';
+    element.style.margin = '10px';
+    element.style.textAlign = 'right';
+    document.getElementById('message-list').appendChild(element);
     this.message = '';
  }
 }
